89084c3d326df65ccc5b496fc01f9d2b6149f5a2
Ogidan Emmanuel <eogidan22@gmail.com>
2022-05-08 09:39:37 +0100

    first commit

---
+++ CourseLibrary.API/Migrations/20220508082957_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseLibrary.API.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfDeath",
+                table: "Authors");
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2aadd2df-7caf-45ab-9355-7f6332985a87"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1723, 4, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2ee49fe3-edf2-4f91-8409-3eb25ce6ca51"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1721, 10, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1650, 7, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1668, 5, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateOfDeath",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2aadd2df-7caf-45ab-9355-7f6332985a87"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1723, 4, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2ee49fe3-edf2-4f91-8409-3eb25ce6ca51"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1721, 10, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1650, 7, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1668, 5, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0)));
+        }
+    }
+}
